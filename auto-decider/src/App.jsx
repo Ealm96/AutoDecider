d0acc6e6230b32cc246102e652acac31e3692f50
Andrew <marionandrew73@gmail.com>
2025-02-03 11:09:02 -0500

    chore(page2): rename page two route

--- auto-decider/src/App.jsx
+++ auto-decider/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/AutoDecider/" index element={<FormPage />} />
-        <Route path='DisplayPage' element={<DisplayPage />} />
+        <Route path='/AutoDecider/DisplayPage' element={<DisplayPage />} />
       </Routes>
     </BrowserRouter>
   )
