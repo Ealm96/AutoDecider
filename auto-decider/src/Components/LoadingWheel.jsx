399e5f491084abc4b2d0fceca08c8791a00f56e8
Andrew <marionandrew73@gmail.com>
2025-06-27 15:03:12 -0400

    feat(DisplayPage): Adds more search options and displays accordingly

--- auto-decider/src/Components/LoadingWheel.jsx
+++ auto-decider/src/Components/LoadingWheel.jsx
@@ -6,7 +6,7 @@ const LoadingWheel = () => {
             <motion.div
                 animate={{ rotate: 360 }}
                 transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
-                className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full"
+                className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full white"
             />
         </div>
     );
